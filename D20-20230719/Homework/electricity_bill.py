769b86d6aea1ec6730dede1dd58ffeb431aa4b5b
reshma1019 <reshmakulapparai@gmail.com>
2023-07-21 10:42:57 +0530

    day 21

--- D20-20230719/Homework/electricity_bill.py
+++ D20-20230719/Homework/electricity_bill.py
@@ -1,49 +1,55 @@
 consumer_data={"consumer_name":"Reshma",
-               "eb_reading":[1100,1200,1350,1650,2050]}
+               "eb_reading":[1100,1200,1350,1650,2050,2567,2768,3124,3345,3689,4567,5423]}
 def calculate_electricity_bill(consumer_data):
- 
+  # print(len("eb_reading"))
   consumer_name=consumer_data["consumer_name"]
-  Eb_reading=consumer_data["eb_reading"]
-  months=[1,2,3,4,5]
-  unit=[]
-  bill_amount=[]
+  eb_reading=consumer_data["eb_reading"]
+
+  
+  for i in range(1,9):
+    
+     unit_consumed= eb_reading[i] - eb_reading[i-1]
+     print(unit_consumed)
+     bill_amount=[]
+  
+     if unit_consumed < 100:
+         bill_amount.append(0)
+      
+     if unit_consumed>=100 and unit_consumed<=200:
+        bill_amount.append(2*unit_consumed)
+      
+     if unit_consumed>=200 and unit_consumed<=500:
+       bill_amount.append(5*unit_consumed)
+
+     if unit_consumed>=500 and unit_consumed<=1000:
+      bill_amount.append(10*unit_consumed)
+
+     if unit_consumed>1000:
+       bill_amount.append(14*unit_consumed)
+  print("bill amount:",bill_amount)
+
+  total_bill=0
+  for bill in bill_amount:
+    total_bill=total_bill+bill
+  print()
+    
+  dict=[{"month":1,
+        "unit_consumed":unit_consumed,
+        "bill amount":bill_amount[0]},
+        {"month":2,
+       "unit_consumed":unit_consumed,
+       "bill amount":bill_amount[1],
+        },
+      {"month":3,
+      "unit_consumed":unit_consumed,
+      "bill amount":bill_amount[2],
+       },
+      {"month":4,
+      "unit_consumed":unit_consumed,
+     "bill amount":bill_amount[3],
+       },
+       ]
+  print("dictionary_view:",dict)
+
  
-  for unit in Eb_reading:
-    if unit < 100:
-      bill_amount.append(0)
-      print(unit,bill_amount)
-    if unit>=100 and unit<=200:
-      bill_amount.append(2*unit)
-      print(unit,bill_amount) 
-    if unit>=200 and unit<=500:
-      bill_amount.append(5*unit)
-    if unit>=500 and unit<=1000:
-      bill_amount.append(10*unit)
-    if unit>1000:
-      bill_amount.append(14*unit)
- 
-  print(months,bill_amount)
-  # total_bill=0
-  # for bill in bill_amount:
-  #   total_bill=total_bill+bill
-  #   print(total_bill)
-  # dict={"consumer name":consumer_name,
-  #      "bill amount":bill_amount,
-  #      "total bill amount":total_bill}
-  # print(dict)
- 
- 
-   
- 
-     
-
-
-
-
-
-
-
-
-
-
 calculate_electricity_bill(consumer_data)
---
+++ D21-20230720/Homework/dict_to_txtfle.py
@@ -0,0 +1,50 @@
+consumer_data={"consumer_name":"Reshma",
+               "eb_reading":[1100,1200,1350,1650,2050,2500,3800]}
+def calculate_electricity_bill(consumer_data):
+
+    eb_reading=consumer_data["eb_reading"]
+    output=[]
+    
+    
+    for i in range(len(eb_reading)-1):
+        unit_consumed=eb_reading[i+1] - eb_reading[i]
+        # print(unit_consumed)
+        month=i+1
+        bill_amount=0
+        
+        if unit_consumed <100:
+            bill_amount=bill_amount+0
+         
+      
+        elif unit_consumed>=100 and unit_consumed<200:
+            bill_amount=bill_amount+(2*unit_consumed)
+      
+        elif unit_consumed>=200 and unit_consumed<500:
+            bill_amount=bill_amount+(5*unit_consumed)
+
+        elif unit_consumed>=500 and unit_consumed<1000:
+            bill_amount=bill_amount+(10*unit_consumed)
+
+        elif unit_consumed>1000:
+            bill_amount=bill_amount+(14*unit_consumed)
+        # print(bill_amount)
+    
+        dict={"Month":month,"unit_consumed":unit_consumed,"bill_amount":bill_amount}
+        output.append(dict)
+    print(output)
+    total=0
+    total=total+bill_amount
+    print("total amount:",total)
+    
+
+    
+    # s=str(output)
+    # print(type(s))
+    text=""
+    for data in output:
+        text=text+f"Month:{data['Month']},Unit_consumed:{data['unit_consumed']},Bill amount:{data['bill_amount']}\n"
+        file_name="/home/reshma/resh.txt"
+        with open(file_name,"w")as file:
+            file.write(text)
+
+calculate_electricity_bill(consumer_data)
